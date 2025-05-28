8af36be143ef2aa7f39c94472095a1347bdaaa49
Gabrielporfiri0 <g.porfirio502@gmail.com>
2025-05-27 21:21:45 -0400

    Quasepronto

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,39 +1,35 @@
-let lastScrollTop =0;
-const header = document.querySelector("header")
-
-window.addEventListener("scroll", function(){
-    let currentScroll = this.window.pageYOffset || this.document.documentElement.scrollTop;
-
-    if(currentScroll > lastScrollTop){
-        header.style.top = '-100px'
+document.addEventListener("DOMContentLoaded", () => {
+    const carousel = document.getElementById("carousel");
+    const slides = document.querySelectorAll(".filme-banner");
+    const prevBtn = document.getElementById("prevBtn");
+    const nextBtn = document.getElementById("nextBtn");
+  
+    let index = 0;
+    const total = slides.length;
+    const intervalTime = 10000; // 5 segundos
+  
+    // Função para atualizar a posição do carrossel
+    function updateCarousel() {
+      const offset = -index * 100;
+      carousel.style.transform = `translateX(${offset}%)`;
     }
-    else{
-        header.style.top = '0'
-    }
-
-    lastScrollTop = currentScroll <= 0 ? 0 : currentScroll 
-})
-
-
-
-
-//carrousel
-const conteiner = document.getElementById("conteiner-img");
-document.getElementById("btn-left").addEventListener("click", function(){
-    conteiner.scrollBy({ left:-580, behavior: "smooth" });
-})
-
-document.getElementById("btn-right").addEventListener("click", function(){
-    conteiner.scrollBy({ left: 580, behavior: "smooth" });
-})
-
-setInterval(()=>{
-    const maxScrollLeft = conteiner.scrollWidth - conteiner.clientWidth;
-
-    if (conteiner.scrollLeft >= maxScrollLeft) {
-        conteiner.scrollTo({left: 0 , behavior:"smooth"})
-    }
-    else{
-        conteiner.scrollBy({left: 580 , behavior:"smooth"})
-    }
-},2500)
+  
+    // Botão próximo
+    nextBtn.addEventListener("click", () => {
+      index = (index + 1) % total;
+      updateCarousel();
+    });
+  
+    // Botão anterior
+    prevBtn.addEventListener("click", () => {
+      index = (index - 1 + total) % total;
+      updateCarousel();
+    });
+  
+    // Auto-play
+    setInterval(() => {
+      index = (index + 1) % total;
+      updateCarousel();
+    }, intervalTime);
+  });
+  
